443c3d3b5b07af087166c9d576ece6b9cb47cc73
daihyun kim <dkim0529@gmail.com>
2019-09-02 18:31:30 +0900

    stpe2

---
+++ src/Movie.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Movie({ year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
