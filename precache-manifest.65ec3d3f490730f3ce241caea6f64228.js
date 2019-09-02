237e8719b007b1ed4823e8a5c022542e9afd2304
daihyun kim <dkim0529@gmail.com>
2019-09-02 17:57:40 +0900

    Updates

---
+++ precache-manifest.65ec3d3f490730f3ce241caea6f64228.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6d847721673dca8c9f9c50f96a0008",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "978ad0eacfe0b80390fd",
+    "url": "/my-app/static/js/2.fb4fcc93.chunk.js"
+  },
+  {
+    "revision": "e2d32ee62ca4cca5be1f",
+    "url": "/my-app/static/js/main.b381a85d.chunk.js"
+  },
+  {
+    "revision": "00fa2de64e66be72c556",
+    "url": "/my-app/static/js/runtime~main.122bc8ec.js"
+  }
+]);
